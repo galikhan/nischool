d3c91d1f9b9df9280114b3565284aca47583fe98
Bekmurat <bekmurat@bekmurat-Lenovo-G500.(none)>
2014-10-24 16:12:50 +0600

    adminka

---
+++ public/media-content/147/theyre_their_there_sco2004/sclb_extras/activity_info.js
@@ -0,0 +1,42 @@
+/**
+ *  ==================================================
+ *  SoftChalk LessonBuilder
+ *  Activity activity_info.js
+ *  Copyright 2008 SoftChalk LLC
+ *  All Rights Reserved.
+ *
+ *  http://www.softchalk.com
+ *  ==================================================
+ */
+
+q_done_a=new Array();
+scoreQa=new Array();
+score_a=new Array();
+show_restart_a=new Array();
+num_pages=5;
+actOrder=new Array(3);
+actOrder[0]=7;
+actOrder[1]=5;
+actOrder[2]=2;
+
+//a_num=7;
+a_value7=3;
+scoreQa[7]=true;
+q_done_a[7]=false;
+score_a[7]=0;
+show_restart_a[7]=false;
+
+//a_num=5;
+a_value5=3;
+scoreQa[5]=true;
+q_done_a[5]=false;
+score_a[5]=0;
+show_restart_a[5]=false;
+
+//a_num=2;
+a_value2=6;
+scoreQa[2]=true;
+q_done_a[2]=false;
+score_a[2]=0;
+show_restart_a[2]=false;
+
